006e3c569b82179692d9bd15ab6e71614ac7f6be
Valentin Despa <valentin@vdespa.com>
2025-08-04 18:09:47 +0200

    Making API calls from E2E tests

--- e2e-tests/example.spec.ts
+++ e2e-tests/example.spec.ts
@@ -1,10 +1,33 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, APIRequestContext } from '@playwright/test';
+
+// Request context
+let apiContext: APIRequestContext;
+
+test.beforeAll(async ({ playwright }) => {
+  apiContext = await playwright.request.newContext({
+    baseURL: 'https://api.valentinos-magic-beans.click',
+    extraHTTPHeaders: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+});
+
+test.afterAll(async ({ }) => {
+  await apiContext.dispose();
+});
 
 test('has title', async ({ page }) => {
+  // Make HTTP request (precondition) - create new order
+  const newOrderResponse = await apiContext.post('/orders'); // simplified
+
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+
+  // Make HTTP request (postcondition)
+  const orderLookUpResponse = await apiContext.post('/orders/lookup'); // simplified
 });
 
 test('get started link', async ({ page }) => {
